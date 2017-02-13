ad29a0a797abdbb3b6385997803e22783d044f27
Latasha Simmons <lsimmons1832@gmail.com>
2017-02-12 19:04:58 -0600

    save work

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
+//capture user input
 var treeHeight = document.getElementById("height").value;
-var charVal = document.getElementById("input").value;
+var treeChar = document.getElementById("character").value;
+
+//define button
+var button = document.getElementById("grow");
+
+//assign function to button
+button.addEventListener("click", buildTree);
+
+function buildTree(){
+	//do math on user input
+var treeArray = [];
+	for (var i = 0; i < treeArray.length; i++) {
+		var treeHeightElement = treeHeight - (i +1);
+	}
+	//build array from math
+	treeArray.push(treeHeightElement);
+}
+console.log(treeArray(i));
+// var treeCharElement = treeChar (2*i) + 1;
