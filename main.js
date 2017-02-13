c1cd9d7fc833793179d150bdf0d21555a2eac919
Latasha Simmons <lsimmons1832@gmail.com>
2017-02-13 13:31:36 -0600

    still need to fix keypress

--- main.js
+++ main.js
@@ -1,42 +1,24 @@
-// //capture user input
-// var treeHeight = document.getElementById("height").value;
-// var treeChar = document.getElementById("character").value;
-
-// //define button
-// var button = document.getElementById("grow");
-
-// //assign function to button
-// button.addEventListener("click", buildTree);
-
-// function buildTree(){
-// 	//do math on user input
-// var treeArray = [];
-// 	for (var i = 0; i < treeArray.length; i++) {
-// 		var treeHeightElement = treeHeight - (i +1);
-// 	}
-// 	//build array from math
-// 	treeArray.push(treeHeightElement);
-// }
-// console.log(treeArray(i));
-// // var treeCharElement = treeChar (2*i) + 1;
-
-	//capture user input
-	var treeChar = document.getElementById("character").value;
-	var treeHeight = document.getElementById("height").value;
+//capture user input
+var treeChar = document.getElementById("character").value;
+var treeHeight = document.getElementById("height").value;
 
 
-//define button
+//get a reference to the button element in the DOM
 var button = document.getElementById("grow");
+
+//capture keypress ***NOT WORKING***
+window.addEventListener("keyPress", 
+	function(e){
+	alert("inside keyPress");
+	var key = e.which || e.keyCode;
+	if (key === 13) {
+		verifyInput();
+	}
+}); 
 
 //call build fuction when button clicked
 button.addEventListener("click", verifyInput);
 
-//capture keypress 
-function clickEvent(keypress){
-	if (keypress.keyCode === 13) {
-		buildTree();
-	}
-}
 
 var treeArray = {
   height: treeHeight.value,
@@ -44,10 +26,9 @@ var treeArray = {
 };
 
 function verifyInput() {
-	alert("We made it here");
-  if (treeArray.height === "") {
+  if (treeHeight === "") {
     alert("Please enter height");
-  		} else if (treeArray.character === "") {
+  		} else if (treeChar === "") {
     alert("Please enter character");
   	} 
   buildTree(treeArray);
@@ -59,12 +40,22 @@ function buildTree(treeArray) {
 	treeArray.character = document.getElementById("character").value;
 	treeArray.height = document.getElementById("height").value;
   var tree = "";
+  var treeString = "";
   console.log(treeArray);
   console.log(treeArray.height);
   //do math on user input
   for (var i = 0; i < treeArray.height; i++) {
     var char = 2 * i + 1;
     var space = treeArray.height - (i + 1);
-    console.log(''.repeat(space) + treeArray.character.repeat(tree));
+  	//add spaces to space variable
+  	for (var j = 0; j < space; j++) {
+  		treeString += " ";
+  	}
+  	  	for (var k = 0; k < char; k++) {
+  			treeString += treeArray.character;
+  		}
+  console.log(treeString);
+  //clear treeString after each loop
+  treeString = "";
   }
 }
